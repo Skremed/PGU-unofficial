7ad8a45d8fc35386e8b04d38d2355897647e8dfb
Kasra <bo.kasra79@gmail.com>
2020-05-28 20:22:08 +0430

    changed setting.py

--- pgu/pgu/settings.py
+++ pgu/pgu/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "vol/web/static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,7 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = 'vol/web/media'
-STATIC_ROOT = 'vol/web/static'
+# STATIC_ROOT = 'vol/web/static'
 STATICFILES_DIRS = [
-    '/vol/web/static/',
+    STATIC_DIR,
 ]
