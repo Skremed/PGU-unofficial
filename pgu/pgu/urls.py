3577f2eeef0c7d9433a6d3e656d76419bd605579
Kasra <bo.kasra79@gmail.com>
2020-05-28 18:04:55 +0430

    changed urls.py

--- pgu/pgu/urls.py
+++ pgu/pgu/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView(template_name="hompage/homepage.html").as_view(), name="")
+    path("", TemplateView.as_view(template_name="hompage/homepage.html"), name="")
 ]
