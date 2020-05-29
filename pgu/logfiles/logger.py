9b939e5e3dbd267a518e9a21288ff05abf43ef0f
Kasra <bo.kasra79@gmail.com>
2020-05-29 20:57:15 +0430

    fixed sibling import for logger.py and scrape.py

--- pgu/logfiles/logger.py
+++ pgu/logfiles/logger.py
@@ -2,7 +2,6 @@ if __name__ == '__main__':
     import os
     import sys
     sys.path.append(os.getcwd())
-
 import time
 import logging
 
--- pgu/scraper/scrape.py
+++ pgu/scraper/scrape.py
@@ -2,7 +2,6 @@ if __name__ == '__main__':
     import os
     import sys
     sys.path.append(os.getcwd())
-
 import os
 import sys
 from selenium import webdriver
